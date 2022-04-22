52672e98f136526dcc7be6dff1a9e05a55027d41
Alejandro <alejomjc2011@gmail.com>
2022-04-22 14:45:53 -0500

    CDA-1 Feat: Se crea el modal y la funcionalidad para invitar y registrar usuarios.

--- Usuarios/urls.py
+++ Usuarios/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from Usuarios.views import views
+from Usuarios.views import views, token
 
 app_name = 'Usuarios'
 
@@ -8,6 +8,8 @@ app_name = 'Usuarios'
 urlpatterns = [
     path('index', views.UsuariosView.as_view(), name='index'),
     path('crear', views.UsuarioCrearView.as_view(), name='crear'),
+    path('invitar', views.UsuarioInvitarView.as_view(), name='invitar'),
     path('editar/<int:id>', views.UsuarioEditarView.as_view(), name='editar'),
     path('eliminar/<int:id>', views.UsuarioEliminarView.as_view(), name='eliminar'),
+    path('token/<str:token>', token.TokenCorreoView.as_view(), name='token'),
 ]
--- Usuarios/views/views.py
+++ Usuarios/views/views.py
@@ -1,16 +1,27 @@
+import random
+
 from django.contrib.auth.models import User, Group
 from django.db import transaction
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.template.loader import get_template
 from django.urls import reverse
+from django.utils.crypto import get_random_string
 
+from ControlDeAcceso.views.mail import enviar_correo
 from ControlDeAcceso.views.mid_auth import AuthAbsView
-from Usuarios.models import Usuario
+from Empresa.models import Empresa
+from Usuarios.models import Usuario, TokenCorreo
 
 
 class UsuariosView(AuthAbsView):
     def get(self, request):
-        usuarios = Usuario.objects.all().exclude(usuario__is_superuser=True)
+        if request.user.is_superuser:
+            usuarios = Usuario.objects.all().exclude(usuario__is_superuser=True,
+                                                     usuario__groups__in=[Group.objects.get(name='Usuario de Empresa')])
+        else:
+            usuarios = Usuario.objects.filter(empresa_id=request.session['empresa_id'])\
+                .exclude(usuario_id=request.user.id)
         return render(request, 'Usuarios/index.html', {'usuarios': usuarios})
 
 
@@ -27,13 +38,15 @@ class UsuarioCrearView(AuthAbsView):
             first_name=rq.get('nombre', ''),
             last_name=rq.get('apellido', ''),
             email=rq.get('correo', ''),
-            username=username
+            username=username.lower()
         )
         usuario.usuario = user
-        rol = Group.objects.get(id=request.POST.get('rol')[0])
-        user.groups.add(rol)
+        if request.user.is_superuser:
+            user.groups.add(Group.objects.get(name='Administrador de Empresa'))
+        else:
+            usuario.empresa_id = request.session['empresa_id']
+            user.groups.add(Group.objects.get(name='Usuario de Empresa'))
         usuario.save()
-
         return redirect(reverse('usuarios:index'))
 
 
@@ -45,17 +58,24 @@ class UsuarioEditarView(AuthAbsView):
     def post(self, request, id):
         rq = request.POST
         usuario = Usuario.data_form_usuario(request.POST)
-        user = User.objects.get(id=id)
-        user.first_name = rq.get('nombre', ''),
-        user.last_name = rq.get('apellido', ''),
-        user.email = rq.get('correo', ''),
-        user.save(update_fields=['first_name', 'last_name', 'email', ])
-
-        usuario.usuario = user
-        user.groups.remove()
-        rol = Group.objects.get(id=request.POST.get('rol')[0])
-        user.groups.add(rol)
+        User.objects.filter(id=id).update(first_name=rq.get('nombre', ''), last_name=rq.get('apellido', ''),
+                                          email=rq.get('correo', ''))
+        usuario.id = Usuario.objects.get(usuario_id=id).id
         usuario.save(update_fields=['empresa_id', 'direccion', 'pais', 'departamento', 'ciudad'])
+        return redirect(reverse('usuarios:index'))
+
+
+class UsuarioInvitarView(AuthAbsView):
+    def get(self, request):
+        return render(request, 'Usuarios/modal_invitar_usuario.html')
+
+    @transaction.atomic
+    def post(self, request):
+        token = get_random_string(length=30)
+        correo = request.POST.get('correo', '')
+        TokenCorreo.objects.create(token=token, ruta='', datos=request.session['empresa_id'])
+
+        enviar_correo({'asunto': 'Invitación a CAD', 'lista_destinatarios': [correo]})
         return redirect(reverse('usuarios:index'))
 
 
@@ -72,8 +92,9 @@ class UsuarioEliminarView(AuthAbsView):
 
 
 def datos_render(id_usuario=None):
+    empresas = [{'id': e.id, 'nombre': e.nombre_empresa} for e in Empresa.objects.all()]
     roles = Group.objects.values('id', 'name')
-    datos = {'roles': roles, 'origen': 'CREAR'}
+    datos = {'roles': roles, 'origen': 'CREAR', 'empresas': empresas}
     if id_usuario:
         datos['usuario'] = Usuario.objects.get(usuario_id=id_usuario)
         datos['origen'] = 'EDITAR'
